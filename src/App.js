ecfe31c5bb219d3e9fbc0bcbe005ebfb7a074ddd
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2018-04-11 15:30:27 -0300

    update withRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Link, Route, BrowserRouter, Redirect, Switch } from 'react-router-dom'
+import { Link, Route, Redirect, Switch, withRouter } from 'react-router-dom'
 
 import Home from './Home'
 import Product from './Product'
@@ -9,27 +9,25 @@ import Page404 from './Page404'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Fragment>
-          <h1>Hello React!</h1>
-          {console.log(this.props.match)}
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/product">Product</Link></li>
-            <li><Link to="/category">Category</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/product" component={Product}/>
-            <Route exact path="/category" component={Category}/>
-            <Route path='/404' component={Page404} />
-            <Route exact path='*' component={Page404} />
-          </Switch>
-        </Fragment>
-      </BrowserRouter>
+      <Fragment>
+        <h1>Hello React!</h1>
+        {console.log(this.props.match)}
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/product">Product</Link></li>
+          <li><Link to="/category">Category</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/product" component={Product}/>
+          <Route exact path="/category" component={Category}/>
+          <Route path='/404' component={Page404} />
+          <Route exact path='*' component={Page404} />
+        </Switch>
+      </Fragment>
     )
   }
 }
 
-export default  App
+export default  withRouter(App)
